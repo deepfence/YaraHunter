84dee45e6ef636313bf0d2b2ea53165fb18405df
Venkata Deviprasad Sura <deviprasad303@gmail.com>
2022-07-15 03:01:02 -0700

    push changes to format

--- output/output.go
+++ output/output.go
@@ -154,26 +154,32 @@ func printColoredIOCJsonObject(IOC IOCFound, isFirstIOC *bool, fileScore float64
 	fmt.Fprintf(os.Stdout, Indent3+"\"Strings to match are\":\n")
 	for _, c := range IOC.StringsToMatch {
 		if len(c) > 0 {
-			fmt.Fprintf(os.Stdout, Indent3+Indent3+"\""+c+"\",\n")
+			fmt.Fprintf(os.Stdout, Indent3+Indent3+"\""+jsonMarshal(c)+"\",\n")
 		}
 	}
 	summary := ""
-	for _, c := range IOC.CategoryName {
+	categoryName := "["
+	for i, c := range IOC.CategoryName {
 		if len(c) > 0 {
 			str := []string{"The file", IOC.CompleteFilename, "has a", c, "match."}
 			summary = strings.Join(str, " ")
+			categoryName = categoryName +"\""+c+"\""
+			if (i==0 && len(IOC.CategoryName)>1) {
+				categoryName = categoryName +","
+			}
 		}
 	}
+	categoryName = categoryName + "]"
 
 	//fmt.Fprintf(os.Stdout, Indent3+"\"String to Match\": %s,\n", IOC.StringsToMatch)
 	//fmt.Fprintf(os.Stdout, Indent3+"\"File Match Severity\": %s,\n", jsonMarshal(severity))
 	//fmt.Fprintf(os.Stdout, Indent3+"\"File Match Severity Score\": %.2f,\n", fileScore)
-	fmt.Fprintf(os.Stdout, Indent3+"\"Category\": %s,\n", IOC.CategoryName)
+	fmt.Fprintf(os.Stdout, Indent3+"\"Category\": %s,\n", jsonMarshal(categoryName))
 	fmt.Fprintf(os.Stdout, Indent3+"\"File Name\": %s,\n", jsonMarshal(IOC.CompleteFilename))
 	for _, c := range IOC.Meta {
 		var metaSplit = strings.Split(c, " : ")
 		if len(metaSplit) > 1 {
-			fmt.Fprintf(os.Stdout, Indent3+"\""+metaSplit[0]+"\": "+metaSplit[1])
+			fmt.Fprintf(os.Stdout, Indent3+"\""+metaSplit[0]+"\":\" "+metaSplit[1]+"\"")
 			if metaSplit[0] == "description" {
 				str := []string{"The file has a rule match that ", strings.Replace(metaSplit[1], "\n", "", -1) + "."}
 				summary = summary + strings.Join(str, " ")
@@ -186,7 +192,7 @@ func printColoredIOCJsonObject(IOC IOCFound, isFirstIOC *bool, fileScore float64
 		}
 	}
 	if len(summary) > 0 {
-		fmt.Fprintf(os.Stdout, Indent3+"\"Summary\": %s,\n", summary)
+		fmt.Fprintf(os.Stdout, Indent3+"\"Summary\": %s,\n", jsonMarshal(summary))
 	}
 
 	fmt.Fprintf(os.Stdout, Indent+Indent+"}\n")
