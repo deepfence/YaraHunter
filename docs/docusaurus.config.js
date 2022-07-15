66f17382df772fba63cba44d0cf6ec4265cefa9b
Owen Garrett <owen.garrett@gmail.com>
2022-07-15 16:39:42 +0100

    github link

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -38,7 +38,7 @@ const config = {
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
           editUrl:
-            'https://github.com/ogarrett/YaRadare/docs/',
+            'https://github.com/deepfence/YaRadare/docs/',
           breadcrumbs: true,
           routeBasePath: '/',
           // options for remark-admonitions
