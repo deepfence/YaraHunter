7d3672e8c3f9e353f4cd6300ce5882e01572309e
Mukul Varshney <72725782+mukuldeepfence@users.noreply.github.com>
2022-12-21 21:29:55 +0530

    adding statement

--- scan/process_image.go
+++ scan/process_image.go
@@ -75,7 +75,7 @@ func (imageScan *ImageScan) extractImage(saveImage bool) error {
 
 	_, err := extractTarFile(imageName, path.Join(tempDir, imageTarFileName), tempDir)
 	if err != nil {
-		core.GetSession().Log.Error("scanImage: Could not extract image tar file: %s", err)
+		core.GetSession().Log.Error("scanImage test: Could not extract image tar file: %s", err)
 		return err
 	}
 
