72d3585e1415530d4ce65d42ccb33f72966cb9db
Harshvardhan Karn <harshvardhan@deepfence.io>
2023-02-16 07:15:15 +0000

    remove commented snippets

--- pkg/scan/scanner.go
+++ pkg/scan/scanner.go
@@ -2,7 +2,6 @@ package scan
 
 import (
 	"github.com/deepfence/YaraHunter/pkg/config"
-	// y "github.com/deepfence/YaraHunter/pkg/yara"
 	yara "github.com/hillu/go-yara/v4"
 )
 
@@ -19,26 +18,3 @@ type Scanner struct {
 func (s *Scanner) SetImageName(imageName string) {
 	s.ImageName = &imageName
 }
-
-/*
-type Options struct {
-    Threads              *int
-    DebugLevel           *string
-    MaximumFileSize      *int64
-    TempDirectory        *string
-    Local                *string
-    HostMountPath        *string
-    ConfigPath           *string
-    OutputPath           *string
-    JsonFilename         *string
-    ImageName            *string
-    MaxIOC               *uint
-    ContainerId          *string
-    ContainerNS          *string
-    SocketPath           *string
-    HttpPort             *string
-    StandAloneHttpPort   *string
-    RulesPath            *string
-    FailOnCompileWarning *bool
-}
-*/
